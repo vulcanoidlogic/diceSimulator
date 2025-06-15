0780a0f673558f9c5b6c294cb76b37bcaea5a577
Greg Fulton <greg.fulton@saic.com>
2025-06-15 07:29:19 -0400

    Refactor analyzeBets function: comment out original implementation for clarity

--- dalembertDice.cjs
+++ dalembertDice.cjs
@@ -287,143 +287,143 @@ const result = analyzeBets(
 );
 
 // Main function to analyze bets based on the given parameters
-async function analyzeBets_ORIGINAL(serverSeed, clientSeed, startNonce, numberOfBets) {
-    let currentStreak = 0;
-    let maxStreak = 0;
-    let maxStreakNonce = 0;
-    let nonce = startNonce;
+// async function analyzeBets_ORIGINAL(serverSeed, clientSeed, startNonce, numberOfBets) {
+//     let currentStreak = 0;
+//     let maxStreak = 0;
+//     let maxStreakNonce = 0;
+//     let nonce = startNonce;
 
-    while (betCount <= totalBets) {
-        bet = nextBet; // Update the bet to the current bet value
-        const roll = getDiceRoll(serverSeed, clientSeed, nonce, 0);
+//     while (betCount <= totalBets) {
+//         bet = nextBet; // Update the bet to the current bet value
+//         const roll = getDiceRoll(serverSeed, clientSeed, nonce, 0);
 
-        if (betHigh) {
-            win = roll >= (100 - chance);
-        } else {
-            win = roll <= chance;
-        }
+//         if (betHigh) {
+//             win = roll >= (100 - chance);
+//         } else {
+//             win = roll <= chance;
+//         }
 
-        if (win) {
-            lastBet = nextBet;
-            nextBet = baseBet;
-            winCount++
-            profit += bet * payOut; // Update profit
-            balance += bet * payOut; // Update balance
-            currentStreak = 0;
-            // Reset current streak or update it as needed
-        } else {
-            lastBet = nextBet;
-            nextBet *= increaseOnLoss;
-            profit -= bet; // Update profit
-            balance -= bet; // Update balance
-            currentStreak++;
-            if (currentStreak > maxStreak) {
-                maxStreak = currentStreak;
-                maxStreakNonce = nonce;
-            }
-        }
-        progress = (betCount  / totalBets) * 100;  // update progress
+//         if (win) {
+//             lastBet = nextBet;
+//             nextBet = baseBet;
+//             winCount++
+//             profit += bet * payOut; // Update profit
+//             balance += bet * payOut; // Update balance
+//             currentStreak = 0;
+//             // Reset current streak or update it as needed
+//         } else {
+//             lastBet = nextBet;
+//             nextBet *= increaseOnLoss;
+//             profit -= bet; // Update profit
+//             balance -= bet; // Update balance
+//             currentStreak++;
+//             if (currentStreak > maxStreak) {
+//                 maxStreak = currentStreak;
+//                 maxStreakNonce = nonce;
+//             }
+//         }
+//         progress = (betCount  / totalBets) * 100;  // update progress
 
-        //if (currentStreak === 9) { // uncooment if you want delayed martingale and set baseBet to 0 or flip bet high and low
-        //    nextBet = 0.0003; 
-        //    //break;
-        //}
+//         //if (currentStreak === 9) { // uncooment if you want delayed martingale and set baseBet to 0 or flip bet high and low
+//         //    nextBet = 0.0003; 
+//         //    //break;
+//         //}
 
-        if (currentStreak === 82) {
-            betHigh = !betHigh;
-        }  
+//         if (currentStreak === 82) {
+//             betHigh = !betHigh;
+//         }  
         
-        //if (currentStreak === 39) {
-        //   betHigh = !betHigh;
-        //} 
+//         //if (currentStreak === 39) {
+//         //   betHigh = !betHigh;
+//         //} 
           
-        if (nextBet > balance) {
-            console.log("Busted!");
-            console.log(
-                win ? '\x1b[32m%s\x1b[0m' : '\x1b[37m%s\x1b[0m',
-                [
-                    'Progress %: ' + progress.toFixed(6),
-                    'Bet Count ' + betCount,
-                    'Server Seed: ' + serverSeed,
-                    'Client Seed: ' + clientSeed,
-                    'Nonce: ' + nonce,
-                    'Roll: ' + roll.toFixed(2),
-                    'Win: ' + win,
-                    'Payout: ' + payOut,
-                    'Bet: ' + bet.toFixed(8),
-                    'Balance: ' + balance.toFixed(8),
-                    'Profit: ' + profit.toFixed(8),
-                    'Total Wagered: ' + totalWagered.toFixed(8),
-                    'Current Streak: ' + currentStreak,
-                    'Bet High: ' + betHigh,
-                    'Worst Streak: ' + maxStreak
-                ].join(' | ')
-                );
-            break;
-        }
+//         if (nextBet > balance) {
+//             console.log("Busted!");
+//             console.log(
+//                 win ? '\x1b[32m%s\x1b[0m' : '\x1b[37m%s\x1b[0m',
+//                 [
+//                     'Progress %: ' + progress.toFixed(6),
+//                     'Bet Count ' + betCount,
+//                     'Server Seed: ' + serverSeed,
+//                     'Client Seed: ' + clientSeed,
+//                     'Nonce: ' + nonce,
+//                     'Roll: ' + roll.toFixed(2),
+//                     'Win: ' + win,
+//                     'Payout: ' + payOut,
+//                     'Bet: ' + bet.toFixed(8),
+//                     'Balance: ' + balance.toFixed(8),
+//                     'Profit: ' + profit.toFixed(8),
+//                     'Total Wagered: ' + totalWagered.toFixed(8),
+//                     'Current Streak: ' + currentStreak,
+//                     'Bet High: ' + betHigh,
+//                     'Worst Streak: ' + maxStreak
+//                 ].join(' | ')
+//                 );
+//             break;
+//         }
 
-        winRatio = (winCount / betCount) * 100;
+//         winRatio = (winCount / betCount) * 100;
 
-        totalWagered += bet; // Update total wagered
+//         totalWagered += bet; // Update total wagered
         
-        if (!debugMode) {
-            if (nonce % 100000 === 0) {
-                const endTime = Date.now();
-                const runTimeSeconds = (endTime - startTime) / 1000;
-                const betsPerSecond = ((nonce - startNonce + 1) / runTimeSeconds).toLocaleString('en-US', { maximumFractionDigits: 2 });
-                const currentNonce = (nonce);
-                const currentSeed = (serverSeed);
+//         if (!debugMode) {
+//             if (nonce % 100000 === 0) {
+//                 const endTime = Date.now();
+//                 const runTimeSeconds = (endTime - startTime) / 1000;
+//                 const betsPerSecond = ((nonce - startNonce + 1) / runTimeSeconds).toLocaleString('en-US', { maximumFractionDigits: 2 });
+//                 const currentNonce = (nonce);
+//                 const currentSeed = (serverSeed);
                 
-                    console.log(
-                        [
-                        'Progress %: ' + progress.toFixed(2),
-                        'Bet Count ' + betCount,
-                        'Max Bets: ' + totalBets,
-                        'Balance: ' + balance.toFixed(4),
-                        'profit: ' + profit.toFixed(2),
-                        'Wins Count: ' + winCount,
-                        'Win Ratio: ' + winRatio.toFixed(2),
-                        'Total Wagered: ' + totalWagered.toFixed(8),
-                        'Worst Loss Streak: ' + maxStreak,
-                        'Bets per Second: ' + betsPerSecond,
-                    ].join(' | ')
-                    );
-            }
-        }   else {
-                 console.log(
-                    win ? '\x1b[32m%s\x1b[0m' : '\x1b[37m%s\x1b[0m',
-                    [
-                        'Server Seed: ' + serverSeed,
-                        'Client Seed: ' + clientSeed,
-                        'Nonce: ' + nonce,
-                        'Progress %: ' + progress.toFixed(4),
-                        'Bet Count ' + betCount,
-                        'Result: ' + roll,
-                        'Bet High: ' + betHigh,
-                        'Next Bet Amount: ' + lastBet.toFixed(5),
-                        'Wagered: ' + totalWagered.toFixed(8),
-                        'profit: ' + profit.toFixed(8),
-                        'Wins: ' + winCount.toFixed(2),
-                        'Balance: ' + balance.toFixed(2),
-                        'Win Ratio: ' + winRatio.toFixed(2),
-                        'Current Streak: ' + currentStreak,
-                        'Worst Streak: ' + maxStreak,
+//                     console.log(
+//                         [
+//                         'Progress %: ' + progress.toFixed(2),
+//                         'Bet Count ' + betCount,
+//                         'Max Bets: ' + totalBets,
+//                         'Balance: ' + balance.toFixed(4),
+//                         'profit: ' + profit.toFixed(2),
+//                         'Wins Count: ' + winCount,
+//                         'Win Ratio: ' + winRatio.toFixed(2),
+//                         'Total Wagered: ' + totalWagered.toFixed(8),
+//                         'Worst Loss Streak: ' + maxStreak,
+//                         'Bets per Second: ' + betsPerSecond,
+//                     ].join(' | ')
+//                     );
+//             }
+//         }   else {
+//                  console.log(
+//                     win ? '\x1b[32m%s\x1b[0m' : '\x1b[37m%s\x1b[0m',
+//                     [
+//                         'Server Seed: ' + serverSeed,
+//                         'Client Seed: ' + clientSeed,
+//                         'Nonce: ' + nonce,
+//                         'Progress %: ' + progress.toFixed(4),
+//                         'Bet Count ' + betCount,
+//                         'Result: ' + roll,
+//                         'Bet High: ' + betHigh,
+//                         'Next Bet Amount: ' + lastBet.toFixed(5),
+//                         'Wagered: ' + totalWagered.toFixed(8),
+//                         'profit: ' + profit.toFixed(8),
+//                         'Wins: ' + winCount.toFixed(2),
+//                         'Balance: ' + balance.toFixed(2),
+//                         'Win Ratio: ' + winRatio.toFixed(2),
+//                         'Current Streak: ' + currentStreak,
+//                         'Worst Streak: ' + maxStreak,
                         
-                    ].join(' | ')
-                    );
-                    // await betDelay(100); // Wait for 100ms before the next iteration
+//                     ].join(' | ')
+//                     );
+//                     // await betDelay(100); // Wait for 100ms before the next iteration
             
-            }
-        nonce++    
-        betCount++    
-    }
+//             }
+//         nonce++    
+//         betCount++    
+//     }
 
-    return {
-        betCount: numberOfBets,
-        maxLossStreak: maxStreak,
-        maxStreakNonce: maxStreakNonce
-    };
-}
+//     return {
+//         betCount: numberOfBets,
+//         maxLossStreak: maxStreak,
+//         maxStreakNonce: maxStreakNonce
+//     };
+// }
 
 // analyzeBets function
 // const result_ORIGINAL = analyzeBets_ORIGINAL(
