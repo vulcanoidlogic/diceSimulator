fab09885f11fc7c33b90871beced07ae3eafdc48
Greg Fulton <greg.fulton@saic.com>
2025-12-24 10:57:17 -0500

    compute max diff

--- scripts/over_under_max_difference_by_chester.py
+++ scripts/over_under_max_difference_by_chester.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the CSV file with proper encoding
-df = pd.read_csv('S:\\SAIC\\Git_projects\\homeProjects\\diceSimulator\\data\\with-trial-sorted_with_outcome.csv', 
+df = pd.read_csv('S:\\SAIC\\Git_projects\\homeProjects\\diceSimulator\\data\\2025-12-18-C7DIFO-MZ--cd29699a63122662e13ef1d5ef0912d6beea12e93429b7ad143ecd802e7a06f6\\with-trial-sorted_with_outcome.csv', 
                  encoding='latin1')
 
 # Calculate absolute difference and find max for each Chester
